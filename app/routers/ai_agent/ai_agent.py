1fa6bd71a56ba455035618a1214d35f095bfe9e8
mortezash <32642757+mortezash@users.noreply.github.com>
2025-12-29 10:45:38 +0330

    fix : response

--- app/routers/ai_agent/ai_agent.py
+++ app/routers/ai_agent/ai_agent.py
@@ -5,6 +5,7 @@ from app.logging_config import get_logger
 
 from app.services.ai_agent.intent_classifier import IntentService
 from app.services.ai_agent.hotel.hotel_filter import HotelFilter
+from app.utils.ai_response_message import success_message
 
 router = APIRouter()
 logger = get_logger("ai-agent")
@@ -32,9 +33,22 @@ def ai_agent(payload: IntentRequest):
             return filter
             #return {"status":200,"message":"OK","detail":"null","data":{"type":"hotel-search","message":"فیلتر برای شهر کیش با تاریخ ورود چهارشنبه 3 دی و تاریخ خروج شنبه 6 دی 1404 ، 1 نفر بزرگسال اعمال شد","result":{"filters":{"city_name":"کیش","check_in":"2025-12-24","check_out":"2025-12-27","passengers":[{"adult":1,"child":[]}],"city_id":760013,"limit":3,"page":1,"hotel":[]}},"requests":["هتل های 4 و 5 ستاره رو برام فیلتر کن","قیمت بین 10 میلیون و 18 میلیون رو برام فیلتر کن","تاریخ ورود رو به پنج شنبه 4 دی تغییر بده","تاریخ ورود رو به جمعه 5 دی تغییر بده","دوتا اتاق میخوام.اتاق دوم سه نفر بزرگسال","شهر اصفهان رو برام فیلتر کن"]},"additional":[],"parameters":[]}
         case 'comparison':
-            return {"status":200,"message":"OK","detail":"null","data":{"type":"hotel-comparison","message":"جزییات داریوش در شهر کیش را میتوانید مشاهده و مقایسه کنید","result":{"hotels":[{"id":1000336,"name_fa":"داریوش"}]},"requests":["هتل های 4 و 5 ستاره رو برام فیلتر کن","قیمت بین 10 میلیون و 18 میلیون رو برام فیلتر کن","تاریخ ورود رو به پنج شنبه 4 دی تغییر بده","تاریخ ورود رو به جمعه 5 دی تغییر بده","دوتا اتاق میخوام.اتاق دوم سه نفر بزرگسال","شهر اصفهان رو برام فیلتر کن"]},"additional":[],"parameters":[]}
+            return success_message(
+                message="جزییات داریوش در شهر کیش را میتوانید مشاهده و مقایسه کنید",
+                type='hotel-comparison',
+                result={
+                    "hotels":[{"id":1000336,"name_fa":"داریوش"}]
+                },
+                request=[]
+            )
         case 'greeting':
-            return {"status":200,"message":"OK","detail":"null","data":{"type":"hotel-comparison","message":"سلام.من تورگردان هستم.در خدمت شما","result":{},"requests":["هتل های 4 و 5 ستاره رو برام فیلتر کن","قیمت بین 10 میلیون و 18 میلیون رو برام فیلتر کن","تاریخ ورود رو به پنج شنبه 4 دی تغییر بده","تاریخ ورود رو به جمعه 5 دی تغییر بده","دوتا اتاق میخوام.اتاق دوم سه نفر بزرگسال","شهر اصفهان رو برام فیلتر کن"]},"additional":[],"parameters":[]}
+            
+            return success_message(
+                message="سلام.من تورگردان هستم.در خدمت شما",
+                type='greeting',
+                result={},
+                request=[]
+            )
         case 'other':
             return {"other": intent}
         case _:
